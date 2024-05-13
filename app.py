b1ceeec30b71a59e0a52a0f3ea5ec4769372e3df
Mbarlowsg <130187725+Mbarlowsg@users.noreply.github.com>
2024-05-13 07:04:24 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+
+df = pd.DataFrame({'flower': ['iris'],
+                   'price': ['$12']})
+
+st.header('Iris Classifier')
+
+st.write("Please enter the measurements of the flower")
+
+sl = st.number_input('Sepal Length')
+sw = st.number_input('Sepal Width')
+pl = st.number_input('Petal Length')
+pw = st.number_input('Petal Width')
+
+
+X = np.array([[sl,sw,pl,pw]])
+
+with open('iris_model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+
+prediction = model.predict(X)
+
+iris_dict = {0: 'Setosa', 1: 'Versicolor', 2: 'Virginica'}
+
+st.write(iris_dict[prediction[0]])
+
+
